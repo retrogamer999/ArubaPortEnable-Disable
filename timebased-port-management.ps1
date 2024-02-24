19e0fe8d1e91a4ed7072d0ab4419adecae43691d
retrogamer999 <64907329+retrogamer999@users.noreply.github.com>
2024-02-24 23:31:42 +0000

    Create timebased-port-management.ps1

---
+++ timebased-port-management.ps1
@@ -0,0 +1,19 @@
+$min = Get-Date '08:00'
+$max = Get-Date '17:30'
+$now = Get-Date
+$Username = "admin"
+$Password = "Spr0cket2011!" | ConvertTo-SecureString -AsPlainText -Force
+$IP = "192.168.40.57"
+
+Import-Module PowerArubaSW
+
+if ($min.TimeOfDay -le $now.TimeOfDay -and $max.TimeOfDay -ge $now.TimeOfDay) {
+    Connect-ArubaSW -SkipCertificateCheck -Username $Username -Password $Password $IP
+    Send-ArubaSWCliBatch -command "int 10", "enable", "wri mem"
+    Disconnect-ArubaSW -Confirm:$false
+}
+else {
+    Connect-ArubaSW -SkipCertificateCheck -Username $Username -Password $Password $IP
+    Send-ArubaSWCliBatch -command "int 10", "disable", "wri mem"
+    Disconnect-ArubaSW -Confirm:$false
+}
